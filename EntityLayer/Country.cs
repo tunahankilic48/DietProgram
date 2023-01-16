a9a18c89c4bedd309afbaed38f8a53c1b2789c00
Tunahan Kilic <tunahankilic942@gmail.com>
2023-01-16 15:45:04 +0300

    Database was updated

---
+++ EntityLayer/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Country : BaseClass
+    {
+        public string? Name { get; set; }
+
+        // Navigation Properties
+        public List<City>? City{ get; set; }
+        public List<Address>? Addresses { get; set; }
+    }
+}
